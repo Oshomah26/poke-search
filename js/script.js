777de4c68e935ae9c1b95da58cc6fe6fcdfa27c5
Oshomah <oshomah26@gmail.com>
2023-04-18 17:26:23 +0100

    Made progress with the fetching of API data from Pokemon TCG site

--- js/script.js
+++ js/script.js
@@ -7,16 +7,26 @@ const global = {
     }
 }
 
+async function displayPopularcards(){
+    // const response = await fetch ('https://api.pokemontcg.io/v2/cards');
+    const results = await fetchAPIData('cards');
+    console.log(results.data);
+}
+
 async function fetchAPIData(endpoint){
     const API_KEY = global.api.apiKey;
     const API_URL = global.api.apiURL;
 
     const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
+    const data = await response.json();
+    
+    return data
 }
 
 function init(){
     switch(global.currentPage){
         case'/index.html':
+        displayPopularcards();
         console.log(global.currentPage);
         break;
         case'/details.html':
