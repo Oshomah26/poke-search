2f8de2db87f98cf94a3f1628a643990ee3e9426b
Oshomah <oshomah26@gmail.com>
2023-04-18 17:00:13 +0100

    Added some lines in terms of page navigation

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 const global = {
     currentPage: window.location.pathname,
+
+    api: {
+        apiKey: '7e6a79da-f0fa-41f5-b7fc-c15b7ed43865',
+        apiURL: 'https://api.pokemontcg.io/v2/'
+    }
+}
+
+async function fetchAPIData(endpoint){
+    const API_KEY = global.api.apiKey;
+    const API_URL = global.api.apiURL;
+
+    const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
 }
 
 function init(){
