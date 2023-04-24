bec807801d263be4951030e55784fdbc54286289
Oshomah <oshomah26@gmail.com>
2023-04-24 18:56:33 +0100

    Created the alert function

--- js/script.js
+++ js/script.js
@@ -114,6 +114,15 @@ async function searchAPIdata(){
     return data;
 }
 
+function showAlert(message, className = 'error'){
+    const alertEl = document.createElement(div);
+    alertEl.classList.add('alert', className);
+    alertEl.appendChild(document.createTextNode(message));
+    document.querySelector('#alert').appendChild(alertEl);
+
+    setTimeout(()=> alertEl.remove(), 3000);
+}
+
 async function fetchAPIData(endpoint){
     const API_KEY = global.api.apiKey;
     const API_URL = global.api.apiURL;
